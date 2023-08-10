cc1e88acc1ec15e4c9eff1a7e0e6d7677e5521f7
Glenelly <glenellykarlatbm@gmail.com>
2023-08-10 14:26:16 -0300

    Enumeradores e constantes (construtor e métodos)

---
+++ src/enumcomoclasse/Formulario.java
@@ -0,0 +1,17 @@
+package enumcomoclasse;
+
+public class Formulario {
+
+    enum Genero{
+        FEMININO('F'), MASCULINO('M');
+        private final char valor;
+        Genero(char valor){
+            this.valor = valor;
+        }
+        public char getValor() {
+            return valor;
+        }
+    }
+    private String nome;
+    private Genero genero;
+}
---
+++ src/enumcomoclasse/Teste.java
@@ -0,0 +1,12 @@
+package enumcomoclasse;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        DiaSemana dia = DiaSemana.SEGUNDA;
+        System.out.println(dia + " - " + dia.getValor());
+
+        Data data = new Data(10,8,2023, DiaSemana.QUINTA);
+
+    }
+}
