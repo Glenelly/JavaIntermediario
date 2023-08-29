54fc4af73b77e026fdd0d05d630fa3a8699262be
Glenelly <glenellykarlatbm@gmail.com>
2023-08-29 19:00:44 -0300

    String: Tokenizer

---
+++ src/string/tokenizer/Tokenizer.java
@@ -0,0 +1,16 @@
+package string.tokenizer;
+
+import java.util.StringTokenizer;
+
+public class Tokenizer {
+    public static void main(String[] args) {
+
+        String doArquivo = "1;Antônio;30;";
+
+        StringTokenizer st = new StringTokenizer(doArquivo, ";");
+
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+    }
+}
