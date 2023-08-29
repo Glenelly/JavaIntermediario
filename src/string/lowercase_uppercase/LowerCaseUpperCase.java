34fefd2e254f082ce6485478c98d52f95c15c49c
Glenelly <glenellykarlatbm@gmail.com>
2023-08-29 17:38:05 -0300

    String: LowerCase UpperCase

---
+++ src/string/lowercase_uppercase/LowerCaseUpperCase.java
@@ -0,0 +1,11 @@
+package string.lowercase_uppercase;
+
+public class LowerCaseUpperCase {
+    public static void main(String[] args) {
+
+        String teste = "Teste";
+        System.out.println(teste.toLowerCase());
+        System.out.println(teste.toUpperCase());
+
+    }
+}
