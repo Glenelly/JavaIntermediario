f26e618252368b6b1c5b4ac9fb53dcbd1e519b39
Glenelly <glenellykarlatbm@gmail.com>
2023-09-01 04:12:16 -0300

    Diferença de datas

--- src/classes_utilitarias/DiferecaDatas.java
+++ src/classes_utilitarias/DiferecaDatas.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
-import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class DiferecaDatas {
