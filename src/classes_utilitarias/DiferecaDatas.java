2a97734263f0cae32e1f32eac1336063a22e0581
Glenelly <glenellykarlatbm@gmail.com>
2023-09-01 04:11:44 -0300

    Diferença de datas

---
+++ src/classes_utilitarias/DiferecaDatas.java
@@ -0,0 +1,87 @@
+package classes_utilitarias;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class DiferecaDatas {
+    public static void main(String[] args) {
+
+        try {
+            diferecaDataAteJava7();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        diferencaDataJava8();
+
+        diferecaDataJava8ComTempo();
+
+        diferencaDataJava8ComChronoUnit();
+
+    }
+
+    public static void diferecaDataAteJava7() throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date primeiraDt = sdf.parse("01/01/2019");
+        Date segundaDt = sdf.parse("01/02/2019");
+
+        long diffEmMil = Math.abs(segundaDt.getTime() - primeiraDt.getTime());
+        long diff = TimeUnit.DAYS.convert(diffEmMil, TimeUnit.MILLISECONDS);
+
+        int dias = (int) (diffEmMil / (1000 * 60 * 60 * 24));
+
+        System.out.println(diff);
+        System.out.println(dias);
+    }
+
+    public static void diferencaDataJava8(){
+
+        LocalDate data1 = LocalDate.of(2019, 7, 6);
+        LocalDate data2 = LocalDate.of(2020, 8, 10);
+
+        Period periodo = Period.between(data1, data2);
+        int dias = periodo.getDays();
+        int meses = periodo.getMonths();
+        int anos = periodo.getYears();
+
+        System.out.println("Dias: " + dias);
+        System.out.println("Meses: " + meses);
+        System.out.println("Anos: " + anos);
+    }
+
+    public static void diferecaDataJava8ComTempo(){
+
+        LocalDateTime data1 = LocalDateTime.of(2019, 1, 1,6,30);
+        LocalDateTime data2 = LocalDateTime.of(2019, 1, 1,8,30);
+
+        Duration duracao = Duration.between(data1 , data2);
+        long diff = duracao.toHours();
+
+        System.out.println(diff);
+    }
+
+    public static void diferencaDataJava8ComChronoUnit(){
+        LocalDateTime data1 = LocalDateTime.of(2019, 1, 1,6,30);
+        LocalDateTime data2 = LocalDateTime.of(2019, 1, 9,8,30);
+
+        long diff = ChronoUnit.HOURS.between(data1, data2);
+        long diffDias = ChronoUnit.DAYS.between(data1,data2);
+
+        System.out.println("Horas: " + diff);
+        System.out.println("Dias: " + diffDias);
+    }
+}
+
+
+
+
+
