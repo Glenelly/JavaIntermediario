31d29726b123d598317a29fc9df2b0444224df2e
Glenelly <glenellykarlatbm@gmail.com>
2023-09-01 14:41:24 -0300

    Números Aleatórios

---
+++ src/classes_utilitarias/NumerosAleatorios.java
@@ -0,0 +1,15 @@
+package classes_utilitarias;
+
+import java.util.Random;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+
+        System.out.println(Math.floor(Math.random() * 100));
+
+        Random aleatorio = new Random();
+
+        System.out.println(aleatorio.nextInt());
+        System.out.println(aleatorio.nextInt(5 + 1));
+    }
+}
